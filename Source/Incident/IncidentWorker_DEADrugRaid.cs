4476e858be12f095010e8f1d13d8cd83af50ea75
Junxian_Luke_Lu <32172402+Luke-lujunxian@users.noreply.github.com>
2024-07-19 21:53:46 +0800

    Fix a issue with RDEA raid executing even friendly

--- Source/Incident/IncidentWorker_DEADrugRaid.cs
+++ Source/Incident/IncidentWorker_DEADrugRaid.cs
@@ -9,14 +9,14 @@ namespace WarOnDrug.Incident
         protected override bool FactionCanBeGroupSource(Faction f, Map map, bool desperate = false)
         {
 
-            if (base.FactionCanBeGroupSource(f, map, desperate) || !f.HostileTo(Faction.OfPlayer))
-            {
-                return f.def.defName == "RIM_DEA";
-            }
+/*            if (base.FactionCanBeGroupSource(f, map, desperate) && f.HostileTo(Faction.OfPlayer))
+            {*/
+                return f.def.defName == "RIM_DEA" && base.FactionCanBeGroupSource(f, map, desperate);
+/*            }
             else
             {
                 return false;
-            }
+            }*/
         }
 
         public override void ResolveRaidStrategy(IncidentParms parms, PawnGroupKindDef groupKind)
